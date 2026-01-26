04b786edf0ec991080b228ab9d59bc7a008533e8
imtiazahmed801 <imtiazya@protonmail.com>
2026-01-26 22:18:34 +0000

    Initial Terraform AWS Platform

---
+++ environments/dev/backend.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "s3" {
+    bucket         = "tf-state-aws-platform-56"
+    key            = "environments/dev/terraform.tfstate"
+    dynamodb_table = "tf-state-locks"
+    region         = "us-east-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source   = "../../modules/vpc"
+  vpc_cidr = "10.0.0.0/16"
+}
