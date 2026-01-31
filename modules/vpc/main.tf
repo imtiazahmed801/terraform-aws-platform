229688f88901658b9b4b8625f790466a8d67aa59
imtiazahmed801 <imtiazya@protonmail.com>
2026-01-31 15:39:57 +0000

    Add ALB, target group and listener

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -97,3 +97,68 @@ resource "aws_route_table_association" "private_subnet_b_assoc" {
   subnet_id = aws_subnet.private_subnet_b.id
   route_table_id = aws_route_table.private_rt.id
 }
+
+resource "aws_security_group" "alb_sg" {
+  name = "alb-sg"
+  description = "Security group for ALB"
+  vpc_id = aws_vpc.vpcblock.id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+    ManagedBy = "Terraform"
+  }
+}
+
+resource "aws_lb" "application_lb" {
+  name = "aws-lb"
+  load_balancer_type = "application"
+  internal = false
+  subnets = [aws_subnet.public_subnet_a.id, aws_subnet.public_subnet_b.id]
+  security_groups = [aws_security_group.alb_sg.id]
+  ip_address_type = "ipv4"
+
+  tags = {
+    Name = "aws-lb"
+    ManagedBy = "Terraform"
+  }
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name = "app-tg"
+  port = 80
+  protocol = "HTTP"
+  target_type = "instance"
+  vpc_id = aws_vpc.vpcblock.id
+  
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+  }
+}
+
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.application_lb.arn
+  port = 80
+  protocol = "HTTP"
+  
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
